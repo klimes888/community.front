e98eeb86ec29c036ee88410dcac61d4a8668cd29
taehee <klimes888@gmail.com>
2022-06-25 09:12:53 +0900

    [ALL] strorybook 패키지 추가

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,24 +1,24 @@
 // react_start/webpack.config.js
-const { merge } = require('webpack-merge')
-const TerserPlugin = require('terser-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const common = require('./webpack.common.js');
+// const { merge } = require('webpack-merge')
+// const TerserPlugin = require('terser-webpack-plugin')
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const common = require('./webpack.common.js');
 
-module.exports = merge(common,{
-  mode:"production",
-  devtool:'cheap-module-source-map',
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'cheap-module-source-map',
   // plugins: [ new CleanWebpackPlugin() ],
-  plugins:[],
+  plugins: [],
   optimization: {
     splitChunks: {
       cacheGroups: {
         commons: {
           test: /[\\/]node_modules[\\/]/,
-          name:'vendors',
-          chunks:'all'
-        }
-      }
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
     },
     minimize: true,
     minimizer: [
@@ -30,6 +30,6 @@ module.exports = merge(common,{
       //     }
       //   }
       // })
-    ]
-  }
+    ],
+  },
 });
